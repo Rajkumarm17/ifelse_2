16befd1c06cbd9b2ebc48b3f408da6643c24f70b
Rajkumar17 <rajkumarm1703@gmail.com>
2024-12-04 19:03:26 +0530

    greater or smaller

---
+++ greater_or_smaller.py
@@ -0,0 +1,7 @@
+# 2 check the number is greater or smaller 
+n1 = int(input("enter a number 1 : "))
+n2 = int(input("enter a number 2 : "))
+if (n1>n2):
+    print("number 1 is greater than number 2")
+else:
+    print("number 1 is less than number 2")
